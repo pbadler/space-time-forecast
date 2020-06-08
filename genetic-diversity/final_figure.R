1dd820926028aad6233d1a2575ba4192fbfff48b
Adler <peter.adler@usu.edu>
2020-06-08 12:36:34 -0600

    revisions round 2

--- genetic-diversity/final_figure.R
+++ genetic-diversity/final_figure.R
@@ -19,14 +19,14 @@ png("figures/dispersal_niche_width.png",height=3.5,width=8.5,res=400,units="in")
   
   #community example
   matplot(saved_weights_comm,type="l",lwd=2,lty=1,col=myCols,
-          xlab="Time step",ylab="Weight")
+          xlab="Time",ylab="Weight")
   legend("topright",legend=d_vals,
        title="dispersal",lwd=2,lty=1,col=myCols,bty="n")
   mtext("(A)",side=3,line=0.5,adj=0)
   
   #eco-evo example
   matplot(saved_weights,type="l",lwd=2,lty=1,col=myCols,
-          xlab="Time step",ylab="Weight")
+          xlab="Time",ylab="Weight")
   legend("topright",legend=fec_Tsigma_baseline[1]*delta_fec_sigma,
          title=expression(sigma[T]),lwd=2,lty=1,col=myCols,bty="n")
   mtext("(B)",side=3,line=0.5,adj=0)
@@ -47,12 +47,12 @@ par(mfrow=c(1,2),tcl=-0.2,mgp=c(2,0.5,0),mar=c(3,4,2,1))
 
 #community example
 matplot(saved_weights_comm,type="l",lwd=2,lty=1,col="black",
-        xlab="Time step",ylab="Weight")
+        xlab="Time",ylab="Weight")
 mtext("(A)",side=3,line=0.5,adj=0)
 
 #eco-evo example
 matplot(saved_weights_reps,type="l",lwd=2,lty=1,col="black",
-        xlab="Time step",ylab="Weight")
+        xlab="Time",ylab="Weight")
 mtext("(B)",side=3,line=0.5,adj=0)
 
 dev.off()
